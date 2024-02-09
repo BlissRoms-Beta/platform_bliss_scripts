4c84cfade4a20520e16aaf9acb2883ede0ca339c
Jis G Jacob <studiokeys@blissroms.org>
2024-02-09 11:20:03 -0500

    scripts: Adapt for BlissROMs
    Change-Id: Ic4968765a53ed91429e1eb35716540a354d6da54
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- default-wallpaper/update_default_wallpaper.sh
+++ default-wallpaper/update_default_wallpaper.sh
@@ -16,19 +16,19 @@ source "../../../build/envsetup.sh"
 convert "${1}" "${PWD}"/default_wallpaper.png
 
 ## Convert the image to all the resolutions we need and put them in the appropriate place
-convert -resize 1080x1080 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-hdpi/default_wallpaper.png
-convert -resize 960x960 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-nodpi/default_wallpaper.png
-convert -resize 1920x1920 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-sw600dp-nodpi/default_wallpaper.png
-convert -resize 1920x1920 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-sw720dp-nodpi/default_wallpaper.png
-convert -resize 1440x1440 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-xhdpi/default_wallpaper.png
-convert -resize 1920x1920 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-xxhdpi/default_wallpaper.png
-convert -resize 2560x2560 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-xxxhdpi/default_wallpaper.png
+convert -resize 1080x1080 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-hdpi/default_wallpaper.png
+convert -resize 960x960 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-nodpi/default_wallpaper.png
+convert -resize 1920x1920 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-mdpi/default_wallpaper.png
+convert -resize 1920x1920 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-ldpi/default_wallpaper.png
+convert -resize 1440x1440 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-xhdpi/default_wallpaper.png
+convert -resize 1920x1920 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-xxhdpi/default_wallpaper.png
+convert -resize 2560x2560 "${PWD}"/default_wallpaper.png "${ANDROID_BUILD_TOP}"/vendor/overlays/rro_overlays/BlissConfigOverlay/res/drawable-xxxhdpi/default_wallpaper.png
 
 ## Cleanup
 rm "${PWD}"/default_wallpaper.png
 
 ## Commit changes
-cd "${ANDROID_BUILD_TOP}"/vendor/evolution && git add rro_overlays/EvolutionXConfigOverlay/res/drawable-* && git commit -m "Update default wallpaper"
+cd "${ANDROID_BUILD_TOP}"/vendor/overlays && git add rro_overlays/BlissConfigOverlay/res/drawable-* && git commit -m "Update default wallpaper"
 
 ## Go back to top
 croot
